3b56078799b2a25e35b683d670d14a6994987ecb
hnthoc <thaihoc2105@gmail.com>
2021-03-22 23:46:39 +0700

    create multiple project success next: refactor root code

---
+++ shopme-web-parent/shopme-backend/src/main/java/com/shopme/site/admin/MainRestController.java
@@ -0,0 +1,15 @@
+package com.shopme.site.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "rest")
+public class MainRestController {
+
+    @GetMapping(value = "")
+    public String defaultRestController() {
+        return "abc";
+    }
+}
